2854761d781950a69f325f5faa9d4ca7c87dd72e
Eric Schwartz <eric.schwartz@theironyard.com>
2017-07-14 10:48:14 -0400

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
+import marvelApp from './reducers'
+
+let store = createStore(marvelApp, applyMiddleware(thunk))
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 });
