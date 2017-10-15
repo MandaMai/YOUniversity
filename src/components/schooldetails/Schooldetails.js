a2ffc528de8755c60c22b28462ef93b675065ad7
Rudy Morosoff <rudy.morosoff@libertymutual.com>
2017-10-15 10:15:25 -0700

    Improved styling for School Details page. Still mocked data.

--- src/components/schooldetails/Schooldetails.js
+++ src/components/schooldetails/Schooldetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, FormGroup, Button, FormControl, ControlLabel, HelpBlock, Link } from 'react-bootstrap';
+import { Grid, Col, Link } from 'react-bootstrap';
 import { browserHistory } from 'react-router';
 import Navigation from '../navigation/Navigation';
 
@@ -63,6 +63,7 @@ let college = {
 
 let schoolURL = 'http://'+college.results[0]["school.school_url"]
 
+
 console.log(college.results[0]["school.name"])
 
 class SchoolDetails extends Component {
@@ -124,78 +125,81 @@ class SchoolDetails extends Component {
 
       <div>
         <Navigation />
+        <div className="details-container">
+          <Col sm={12} md={12} lg={12} className="details-title">
+            <h1>{college.results[0]["school.name"]}</h1>
+          </Col>
 
-        <div className="details-title">
-          <h1>{college.results[0]["school.name"]}</h1>
-        </div>
+          <Col sm={12} md={6} lg={6} className="details-summary details-card">
+            <h2>School Summary</h2>
+            <p>Website URL: <a href={schoolURL} target="_blank">{college.results[0]["school.school_url"]}</a></p>
+            <p>State:  {college.results[0]["school.state"]}</p>
+            <p>City:  {college.results[0]["school.city"]}</p>
+            <p>School size (Enrollement):  {college.results[0]["2015.student.size"]}</p>
+            <p>Highest Degree Offered:  {getDegree[college.results[0]["school.degrees_awarded.highest"]]}</p>
+            <p>Predominant Degree Offered:  {getPredominant[college.results[0]["school.degrees_awarded.predominant"]]}</p>
+            <p>Institution Type:  {getOwnership[college.results[0]["school.ownership"]]}</p>
+            <p>Locale Description:  {getLocale[college.results[0]["school.locale"]]}</p>
+          </Col>
 
-        <div className="details-summary details-card">
-          <h2>School Summary</h2>
-          <p>Website URL: <a href={schoolURL}>{college.results[0]["school.school_url"]}</a></p>
-          <p>State:  {college.results[0]["school.state"]}</p>
-          <p>City:  {college.results[0]["school.city"]}</p>
-          <p>School size (Enrollement):  {college.results[0]["2015.student.size"]}</p>
-          <p>Highest Degree Offered:  {getDegree[college.results[0]["school.degrees_awarded.highest"]]}</p>
-          <p>Predominant Degree Offered:  {getPredominant[college.results[0]["school.degrees_awarded.predominant"]]}</p>
-          <p>Institution Type:  {getOwnership[college.results[0]["school.ownership"]]}</p>
-          <p>Locale Description:  {getLocale[college.results[0]["school.locale"]]}</p>
-        </div>
+          <Col sm={12} md={6} lg={6} className="student-data details-card">
+            <h2>Student Data</h2>
+            <p>Percentage Women: {Math.round((college.results[0]["2015.student.demographics.women"])*100)}%</p>
+            <p>Percentage Men: {Math.round((college.results[0]["2015.student.demographics.men"])*100)}%</p>
+            <p>Average SAT Score: {college.results[0]["2015.admissions.sat_scores.average.overall"]}</p>
+            <p>Acceptence Rate: {Math.round((college.results[0]["2015.admissions.admission_rate.overall"])*100)}%</p>
+            <p>Number of Grad Students: {college.results[0]["2015.student.grad_students"]}</p>
+            <p>Retention Rate: {Math.round((college.results[0]["2015.student.retention_rate.four_year.full_time"])*100)}%</p>
+            <p>Completion Rate: {Math.round((college.results[0]["2015.completion.completion_rate_4yr_150nt"])*100)}%</p>
+            <p>Median Family Income: ${college.results[0]["2015.student.demographics.median_family_income"]}</p>
+          </Col>
 
-        <div className="student-data details-card">
-          <h2>Student Data</h2>
-          <p>Percentage Women: {Math.round((college.results[0]["2015.student.demographics.women"])*100)}%</p>
-          <p>Percentage Men: {Math.round((college.results[0]["2015.student.demographics.men"])*100)}%</p>
-          <p>Average SAT Score: {college.results[0]["2015.admissions.sat_scores.average.overall"]}</p>
-          <p>Acceptence Rate: {Math.round((college.results[0]["2015.admissions.admission_rate.overall"])*100)}%</p>
-          <p>Number of Grad Students: {college.results[0]["2015.student.grad_students"]}</p>
-          <p>Retention Rate: {Math.round((college.results[0]["2015.student.retention_rate.four_year.full_time"])*100)}%</p>
-          <p>Completion Rate: {Math.round((college.results[0]["2015.completion.completion_rate_4yr_150nt"])*100)}%</p>
-          <p>Median Family Income: ${college.results[0]["2015.student.demographics.median_family_income"]}</p>
-        </div>
+          <Col sm={12} md={6} lg={6} className="financial-data details-card">
+            <h2>Financial Data</h2>
+            <p>Tuition In-State: ${college.results[0]["2015.cost.tuition.in_state"]}</p>
+            <p>Tuition Out-of-State: ${college.results[0]["2015.cost.tuition.out_of_state"]}</p>
+            <p>Cost of Attendance: ${college.results[0]["2015.cost.attendance.academic_year"]}</p>
+            <p>Average Net Price: ${college.results[0]["2015.cost.avg_net_price.overall"]}</p>
+            <p>Average Debt Upon Completion: ${college.results[0]["2015.aid.median_debt.completers.overall"]}</p>
+            <p>Percentage of Students with Federal Loans: {Math.round((college.results[0]["2015.aid.federal_loan_rate"])*100)}%</p>
+            <p>Percentage of Students with Pell Grants: {Math.round((college.results[0]["2015.aid.pell_grant_rate"])*100)}%</p>
+          </Col>
 
-        <div className="financial-data details-card">
-          <h2>Financial Data</h2>
-          <p>Tuition In-State: ${college.results[0]["2015.cost.tuition.in_state"]}</p>
-          <p>Tuition Out-of-State: ${college.results[0]["2015.cost.tuition.out_of_state"]}</p>
-          <p>Cost of Attendance: ${college.results[0]["2015.cost.attendance.academic_year"]}</p>
-          <p>Average Net Price: ${college.results[0]["2015.cost.avg_net_price.overall"]}</p>
-          <p>Average Debt Upon Completion: ${college.results[0]["2015.aid.median_debt.completers.overall"]}</p>
-          <p>Percentage of Students with Federal Loans: {Math.round((college.results[0]["2015.aid.federal_loan_rate"])*100)}%</p>
-          <p>Percentage of Students with Pell Grants: {Math.round((college.results[0]["2015.aid.pell_grant_rate"])*100)}%</p>
-        </div>
-
-        <div className="financial-breakdown details-card">
-          <h2>Net Price by Family Income</h2>
-          <table>
-            <thead>
-              <tr>
-                <td>Family Income Range</td>
-                <td>Net Price for Range</td>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>$0 to $30000</td>
-                <td>${(college.results[0]["2015.cost.net_price.public.by_income_level.0-30000"]) ? college.results[0]["2015.cost.net_price.public.by_income_level.0-30000"] : college.results[0]["2015.cost.net_price.private.by_income_level.0-30000"]}</td>
-              </tr>
-              <tr>
-                <td>$300001 to $48000</td>
-                <td>${(college.results[0]["2015.cost.net_price.public.by_income_level.30001-48000"]) ? college.results[0]["2015.cost.net_price.public.by_income_level.30001-48000"] : college.results[0]["2015.cost.net_price.private.by_income_level.30001-48000"]}</td>
-              </tr>
-              <tr>
-                <td>$48001 to $75000</td>
-                <td>${(college.results[0]["2015.cost.net_price.public.by_income_level.48001-75000"]) ? college.results[0]["2015.cost.net_price.public.by_income_level.48001-75000"] : college.results[0]["2015.cost.net_price.private.by_income_level.48001-75000"]}</td>
-              </tr>
-              <tr>
-                <td>$75000 to $110000</td>
-                <td>${(college.results[0]["2015.cost.net_price.public.by_income_level.75001-110000"]) ? college.results[0]["2015.cost.net_price.public.by_income_level.75001-110000"] : college.results[0]["2015.cost.net_price.private.by_income_level.75001-110000"]}</td>
-              </tr>
-              <tr>
-                <td>More than $110000</td>
-                <td>${(college.results[0]["2015.cost.net_price.public.by_income_level.110001-plus"]) ? college.results[0]["2015.cost.net_price.public.by_income_level.110001-plus"] : college.results[0]["2015.cost.net_price.private.by_income_level.110001-plus"]}</td>
-              </tr>
-            </tbody>
-          </table>
+          <Col sm={12} md={6} lg={6} className="financial-breakdown details-card">
+            <h2>Net Price by Family Income</h2>
+            <div className="table-container">
+              <table className="table table-striped">
+                <thead>
+                  <tr>
+                    <td>Family Income Range</td>
+                    <td>Net Price for Range</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>$0 to $30000</td>
+                    <td>${(college.results[0]["2015.cost.net_price.public.by_income_level.0-30000"]) ? college.results[0]["2015.cost.net_price.public.by_income_level.0-30000"] : college.results[0]["2015.cost.net_price.private.by_income_level.0-30000"]}</td>
+                  </tr>
+                  <tr>
+                    <td>$300001 to $48000</td>
+                    <td>${(college.results[0]["2015.cost.net_price.public.by_income_level.30001-48000"]) ? college.results[0]["2015.cost.net_price.public.by_income_level.30001-48000"] : college.results[0]["2015.cost.net_price.private.by_income_level.30001-48000"]}</td>
+                  </tr>
+                  <tr>
+                    <td>$48001 to $75000</td>
+                    <td>${(college.results[0]["2015.cost.net_price.public.by_income_level.48001-75000"]) ? college.results[0]["2015.cost.net_price.public.by_income_level.48001-75000"] : college.results[0]["2015.cost.net_price.private.by_income_level.48001-75000"]}</td>
+                  </tr>
+                  <tr>
+                    <td>$75000 to $110000</td>
+                    <td>${(college.results[0]["2015.cost.net_price.public.by_income_level.75001-110000"]) ? college.results[0]["2015.cost.net_price.public.by_income_level.75001-110000"] : college.results[0]["2015.cost.net_price.private.by_income_level.75001-110000"]}</td>
+                  </tr>
+                  <tr>
+                    <td>More than $110000</td>
+                    <td>${(college.results[0]["2015.cost.net_price.public.by_income_level.110001-plus"]) ? college.results[0]["2015.cost.net_price.public.by_income_level.110001-plus"] : college.results[0]["2015.cost.net_price.private.by_income_level.110001-plus"]}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </Col>
         </div>
 
       </div>
