e47bad6662ae1c22a46e2f55ad590cde8a5a8f7e
MandaMai <a.maifeld@hotmail.com>
2017-10-16 09:22:59 -0700

    adding files for favorites list

---
+++ src/components/userFavorites/UserFavorites.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+// import SearchResults from '../searchResults/SearchResults';
+import Navigation from '../navigation/Navigation';
+
+import CurrentPreferences from '../currentpreferences/CurrentPreferences';
+import Schools from '../../containers/School';
+import './Dashboard.css';
+
+class UserFavorites extends Component {
+  render() {
+
+    return (
+      <div>
+        
+        <Navigation />
+        <h2 className="welcome">Hey there, INSERT NAME HERE</h2>
+        {/* <CurrentPreferences /> */}
+        {/* <SearchResults /> */}
+        <Favorites />
+
+      </div>
+    );
+  }
+}
+
+export default UserFavorites;
